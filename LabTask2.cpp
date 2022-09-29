68dd4f425867321532663113f19a989069f1111c
shoileee <114634305+shoileee@users.noreply.github.com>
2022-09-29 22:30:23 +0600

    Add files via upload

---
+++ LabTask2.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int i;
+    cin>>i;
+
+    if (i%2==0)
+    {
+        cout<<"The number is even"<<endl;
+    }
+    else
+    {
+        cout<<"The number is odd"<<endl;
+    }
+}
